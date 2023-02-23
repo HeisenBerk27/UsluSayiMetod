4cc9328ea7c25382fe0831cc30c251caf8a6defb
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-23 19:17:00 +0300

    Add files via upload

---
+++ src/metodlar/UsluSayiMetod.java
@@ -0,0 +1,29 @@
+package metodlar;
+import java.util.Scanner;
+
+public class UsluSayiMetod {
+	
+	static int Üst(int a,int b) {
+		if(b==0) {
+			return 1;
+		}
+		else {
+			return a * Üst(a,b-1);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Tabanı giriniz: ");
+		int a= scanner.nextInt();
+		System.out.println("Üstü giriniz: ");
+		int b = scanner.nextInt();
+		
+		int result = Üst(a,b);
+		System.out.println(a + " Üzeri " +b + " = " + result);
+	}
+
+}
